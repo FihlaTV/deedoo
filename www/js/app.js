5f11af8cf01a274c457ae499db8e9fa58cee416a
Le Tyrant Mathieu <mathieu@HelloWorld.local>
2014-12-11 13:15:53 +0100

    [MOBILE] - Add the structure app
    With Slider tutorial / Home (Subscribe & Login) / And basic page with
    menu

---
+++ www/js/app.js
@@ -0,0 +1,68 @@
+angular.module('deedoo', ['ionic', 'firebase'])
+
+    /*
+     * When Mobile Running
+     */
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+    /*
+     * CONFIG
+     */
+    .constant('config', {
+        'appFirebaseName': 'deedoo',
+        'firebaseUrl'    : 'https://' + this.appFirebaseName + '.firebaseio.com/',
+        'intro'          : false
+    })
+
+    /*
+     * Route System
+     */
+    .config(function ($stateProvider, $urlRouterProvider, config) {
+        $stateProvider
+            // Menu
+            .state('tab', {
+                url        : "/tab",
+                templateUrl: "templates/tabs.html"
+            })
+
+            // Tutorial when we start app for the first time
+            .state('tutorial', {
+                url  : '/tutorial',
+                views: {
+                    '': {
+                        templateUrl: 'templates/tutorial.html'
+                    }
+                }
+            })
+
+            // Home page with Subscribe & Login
+            .state('home', {
+                url  : '/home',
+                views: {
+                    '': {
+                        templateUrl: 'templates/home.html'
+                    }
+                }
+            })
+
+            .state('tab.test', {
+                url: '/test',
+                views: {
+                    'tab-test@tab': {
+                        templateUrl: 'templates/tab-test.html'
+                    }
+                }
+            });
+
+        var otherwise = (!config.intro) ? '/tutorial' : '/home';
+        $urlRouterProvider.otherwise(otherwise);
+        
+    });
