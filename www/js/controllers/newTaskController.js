6bbe7191751c29015dc4de96b684272d28486c3a
Mathieu <mathieu.letyrant@gmail.com>
2014-12-12 19:37:37 +0100

    First step for NewTask
    List of babysitters works

--- www/js/controllers/newTaskController.js
+++ www/js/controllers/newTaskController.js
@@ -1,5 +1,20 @@
 'use strict';
 
-angular.module('deedoo').controller('newTaskController', function ($scope){
+angular.module('deedoo').controller('newTaskController', function ($scope, $firebase, config){
 
-});
+    var ref         = new Firebase(config.firebaseUrl + 'MEMBERS'),
+        sync        = $firebase(ref),
+        members     = $firebase(ref).$asArray();
+
+    // List of Babysitters
+    $scope.babysitters = [];
+
+    members.$loaded().then(function (result) {
+        for (var i = 0; i < result.length; i++) {
+            if(members[i].type == 'babysitter'){
+                $scope.babysitters.push(members[i]);
+            }
+        }
+    });
+
+})
