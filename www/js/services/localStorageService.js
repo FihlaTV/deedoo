0c9888c7d0591529fb23ab1d29c6daa681665f78
Mathieu <mathieu.letyrant@gmail.com>
2014-12-14 14:08:05 +0100

    Fix localStorage for Cordova/Ionic

--- www/js/services/localStorageService.js
+++ www/js/services/localStorageService.js
@@ -1,13 +1,13 @@
 'use strict';
 
-angular.module('deedoo').factory('localStorage', function () {
+angular.module('deedoo').factory('localStorage', function ($window) {
 
     return {
         setTutorial: function (value) {
-            window.localStorage['deedoo.tutorial'] = value;
+            $window.localStorage['deedoo.tutorial'] = value;
         },
         getTutorial: function () {
-            return window.localStorage['deedoo.tutorial'];
+            return $window.localStorage['deedoo.tutorial'];
         }
     }
 
