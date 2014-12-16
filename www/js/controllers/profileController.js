dda419ad832cf015900d73942edb28f5711dd1d6
Mathieu <mathieu.letyrant@gmail.com>
2014-12-16 12:14:38 +0100

    Fix ProfilController and Scroll on connect

--- www/js/controllers/profileController.js
+++ www/js/controllers/profileController.js
@@ -5,26 +5,25 @@ angular.module('deedoo').controller('profileController', function ($rootScope, $
     /*
      * GetMember
      */
-    var ref         = new Firebase(config.firebaseUrl + 'MEMBERS/' + config.user.$id),
-        syncMember  = $firebase(ref);
+    var ref = new Firebase(config.firebaseUrl + 'MEMBERS/' + config.user.$id),
+        syncMember = $firebase(ref);
 
 
-    $scope.$watch('profilData', function()
-    {
+    $scope.$watch('profilData', function () {
 
-        syncMember.$update({
-            'firstname': $scope.profilData.firstname,
-            'lastame': $scope.profilData.lastname,
-            'mail': $scope.profilData.mail,
-            'password' : $filter('hash')(scope.profilData.password + config.sold),
-            'phone': $scope.profilData.phone,
-            'type': (parent) ? 'babysitter' : 'parent'
-        }).then(function()
-        {
-            // config.user à modifier avec les nouvelles valeurs de l'utlisateurs
-            console.log('hey');
-        });
-    }
+            syncMember.$update({
+                'firstname': $scope.profilData.firstname,
+                'lastame'  : $scope.profilData.lastname,
+                'mail'     : $scope.profilData.mail,
+                'password' : $filter('hash')(scope.profilData.password + config.sold),
+                'phone'    : $scope.profilData.phone,
+                'type'     : (parent) ? 'babysitter' : 'parent'
+            }).then(function () {
+                // config.user à modifier avec les nouvelles valeurs de l'utlisateurs
+                console.log('hey');
+            });
+
+        }
     );
 
 
