2964c929559d5a214392a161e2062db7060759d6
Mikaoo <vong.bernard@gmail.com>
2014-12-16 11:06:24 +0100

    profile controller

---
+++ www/js/controllers/profileController.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('deedoo').controller('profileController', function ($rootScope, $scope, $state, $firebase, config, $filter) {
+
+    /*
+     * GetMember
+     */
+    var ref         = new Firebase(config.firebaseUrl + 'MEMBERS/' + config.user.$id),
+        syncMember  = $firebase(ref);
+
+
+    $scope.$watch('profilData', function()
+    {
+
+        syncMember.$update({
+            'firstname': $scope.profilData.firstname,
+            'lastame': $scope.profilData.lastname,
+            'mail': $scope.profilData.mail,
+            'password' : $filter('hash')(scope.profilData.password + config.sold),
+            'phone': $scope.profilData.phone,
+            'type': (parent) ? 'babysitter' : 'parent'
+        }).then(function()
+        {
+            // config.user à modifier avec les nouvelles valeurs de l'utlisateurs
+            console.log('hey');
+        });
+    }
+    );
+
+
+});
