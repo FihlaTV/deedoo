1659aec39ad38072a5ee1949fc67372a36b05968
Mikaoo <vong.bernard@gmail.com>
2014-12-17 22:41:15 +0100

    Add data in guard_details

--- www/js/controllers/informationGuardController.js
+++ www/js/controllers/informationGuardController.js
@@ -1,5 +1,12 @@
 'use strict';
 
+
+/*
+ *  Controller of informations_guard.html 
+ *  --> Page for: BABYSITTER | display informations about 1 new guard
+*/ 
+
+
 angular.module('deedoo').controller('informationGuardController', function ($rootScope, $scope, $state, $stateParams, $firebase, config) {
 
     /*
@@ -9,6 +16,50 @@ angular.module('deedoo').controller('informationGuardController', function ($roo
         $state.go('connect');
         return;
     }
+
+    /*
+     * Firebase room & tasks
+     */
+    var refGuard    = new Firebase(config.firebaseUrl + 'ROOM/' + $stateParams.idGuard),
+        room        = $firebase(refGuard).$asObject();
+
+    /*
+     * Informations guard
+     */
+    room.$loaded().then(function (result) {
+        var mois            = ['janvier','février','mars','avril','mai','juin',
+                              'juillet','août','septembre','octobre','novembre','décembre'],
+            dateGuard       = new Date(result.date),
+            childrens       = '',
+            first           = true;
+
+
+        result.children.forEach(function(element, key, array)
+        {
+            if(first)
+            {
+                childrens += element.name;
+                first = false;
+            }
+            else
+                childrens += ', ' + element.name;
+        });
+
+        $scope.data = 
+        {
+            'firstname_parent'    : result.firstname_parent,
+            'lastname_parent'     : result.lastname_parent,
+            'time_beginning'      : result.time_beginning,
+            'time_ending'         : result.time_ending,
+            'date'                : dateGuard.getDate() + ' '
+                                    + mois[dateGuard.getMonth()] + ' '
+                                    + dateGuard.getFullYear(),
+            'childrens'           : childrens
+        };
+
+        console.log($scope.data);
+
+    });
 
     $scope.confirm = function (booleen) {
         if (booleen) {
